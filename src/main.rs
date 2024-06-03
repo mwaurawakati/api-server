439ff212883eb39ce01d6954d4ddfe4f0a2cee4f
Mwaura Wakati <davewakati@gmail.com>
2024-06-03 18:13:47 +0300

    initial commit serverless

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use jemallocator::Jemalloc;
 #[global_allocator]
 static GLOBAL: Jemalloc = Jemalloc;
 
-#[rocket::main]
+/*#[rocket::main]
 async fn main() -> Result<(), Error> {
     // start the server
     match init_server().await {
@@ -19,4 +19,31 @@ async fn main() -> Result<(), Error> {
             exit(1)
         }
     }
+}*/
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        // disable printing the name of the module in every log line.
+        .with_target(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
+
+    let bucket_name = "mwaurawakati"; /*std::env::var("BUCKET_NAME")
+        .expect("A BUCKET_NAME must be set in this app's Lambda environment variables.");*/
+
+    // Initialize the client here to be able to reuse it across
+    // different invocations.
+    //
+    // No extra configuration is needed as long as your Lambda has
+    // the necessary permissions attached to its role.
+    let config = aws_config::load_defaults(BehaviorVersion::latest()).await;
+    let s3_client = aws_sdk_s3::Client::new(&config);
+
+    lambda_runtime::run(service_fn(|event: LambdaEvent<Request>| async {
+        put_object(&s3_client, &bucket_name, event).await
+    }))
+    .await
 }
